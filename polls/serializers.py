756ffff7af71e76e91a0ee1480fc26ca5827bea2
fk03983 <fk03983@st.habib.edu.pk>
2021-03-14 04:44:45 +0500

    updated serializer

--- polls/serializers.py
+++ polls/serializers.py
@@ -4,7 +4,10 @@ from rest_framework import serializers
 
 class DataEntrySerializer(serializers.Serializer):
     char = serializers.CharField(max_length= 255, required= True)
-    data = serializers.ListField(child=serializers.ListField(child= serializers.IntegerField()))
+    data = serializers.ListField(
+        child=serializers.ListField(
+            child = serializers.ListField(
+            child= serializers.IntegerField())))
 
 class EmptySerializer(serializers.Serializer):
     pass
