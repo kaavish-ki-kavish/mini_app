bcda99c88d8140badc0c69865d069d181a5126db
Mudasir Hanif <ms03831@st.habib.edu.pk>
2021-03-19 20:23:41 +0500

    upadted

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('data/', include('polls.urls')),
+    path('models/', include('polls.urls')),
 ]
