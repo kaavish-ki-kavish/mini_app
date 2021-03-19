489926db2b10a3894d68c71b67937a0b09ecdfc2
Mudasir Hanif <ms03831@st.habib.edu.pk>
2021-03-19 20:35:53 +0500

    updated

--- polls/serializers.py
+++ polls/serializers.py
@@ -11,3 +11,20 @@ class DataEntrySerializer(serializers.Serializer):
 
 class EmptySerializer(serializers.Serializer):
     pass
+
+class DataEntrySerializerTf(serializers.Serializer):
+    char = serializers.CharField(max_length= 255, required= True)
+    img = serializers.ListField(
+        child=serializers.ListField(
+            child= serializers.IntegerField())
+    )
+    whole_x = serializers.ListField(
+                child= serializers.IntegerField()
+            )
+    whole_y = serializers.ListField(
+                child= serializers.IntegerField()
+            )
+    pen_up = serializers.ListField(
+                child= serializers.IntegerField()
+            )
+    exercise = serializers.IntegerField()
